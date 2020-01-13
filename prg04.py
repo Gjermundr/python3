836872682787a3c2ee24abf3071ad74fd103dda4
Gjermundr <45577045+Gjermundr@users.noreply.github.com>
2020-01-13 12:28:19 +0100

    updating repo

--- prg04.py
+++
@@ -1,5 +0,0 @@
-asciletter = input('give me a letter you want the ASCII code for: ')
-print('The ASCII code for the letter '+ asciletter+' is: '+ str(ord(asciletter)))
-
-symtrans = input('give me a number value to translate into its corresponding character symbol: ')
-print('The number '+str(symtrans)+' corresponds to the character symbol: '+chr(int(symtrans)))
