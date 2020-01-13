f90cb4365fbe8ee32b5e32ad87bc13e34b3000d6
gjermund.haugen <gjermund.f.haugen@gmail.com>
2020-01-13 10:23:37 +0100

    yes

---
+++ prg_mod1_l4.py
@@ -0,0 +1,5 @@
+asciletter = input('give me a letter you want the ASCII code for: ')
+print('The ASCII code for the letter '+ asciletter+' is: '+ str(ord(asciletter)))
+
+symtrans = input('give me a number value to translate into its corresponding character symbol: ')
+print('The number '+str(symtrans)+' corresponds to the character symbol: '+chr(int(symtrans)))
