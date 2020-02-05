55213bf9168f2f4f6101274a3aa56a298bef845a
gjermund.haugen <gjermund.f.haugen@gmail.com>
2020-02-05 11:06:24 +0100

    updated

--- ShapeManager.py
+++ ShapeManager.py
@@ -1,45 +1,51 @@
 class Shape:
-    _counter = 0
-    _shape = 'Shape'
+    '''base class for all child-classses/objects'''
+    _counter = 0                         # A counter for each object created
+    _shape = 'Shape'                     # Class variable declaring type of object/shape
 
     def __init__(self, description):
         self.description = description
-        Shape._counter += 1
+        Shape._counter += 1             # <- Statement that increments the class variable each time constructor is called
 
     def area(self):
+        '''The areal calculation of object'''
         return 0
 
     def volume(self):
+        '''the volume calculation of object'''
         return 0
 
     def __str__(self):
-        return f'Object: {self._shape} \nDescription: {self.description} \nArea of object: {self.area()} \nVolume of object: {self.volume()}\n'
+        '''layout for all sub-classes and objects with shape, description, area, volume'''
+        return f'Object: {self._shape} \nDescription: {self.description}' \
+               f' \nArea of object: {self.area()} \nVolume of object: {self.volume()}\n'
 
 
 class TwoDimensionalShape(Shape):
-    _shape = 'Two-dimensional Shape'
+    _shape = 'Two-dimensional Shape'            # Updating class variable with correct name
 
     def __init__(self, description):
         super().__init__(description)
 
     def __str__(self):
-        return super().__str__()
+        return super().__str__()                # inherit __str__ of base class: Shape
 
 
 class ThreeDimensionalShape(Shape):
-    _shape = 'Three-Dimensional Shape'
+    _shape = 'Three-Dimensional Shape'          # Updating class variable with correct name
 
     def __init__(self, description):
         super().__init__(description)
 
     def __str__(self):
-        return super().__str__()
+        return super().__str__()                # inherit __str__ of base class: Shape
 
 
 class Square(TwoDimensionalShape):
     _shape = 'Square'
 
     def __init__(self, description, length: int):
+        '''The square's sides are all equal lengths'''
         self.length = length
         super().__init__(description)
 
@@ -54,6 +60,7 @@ class Rectangle(TwoDimensionalShape):
     _shape = 'Rectangle'
 
     def __init__(self, description, length: int, width: int):
+        '''A rectangle has two unique side values'''
         self.length = length
         self.width = width
         super().__init__(description)
@@ -69,6 +76,7 @@ class Cube(ThreeDimensionalShape):
     _shape = 'Cube'
 
     def __init__(self, description, length: int):
+        '''The cube is three-dimensional with all sides being the same length'''
         self.length = length
         super().__init__(description)
 
@@ -87,6 +95,7 @@ class Box(ThreeDimensionalShape):
     _shape = 'Box'
 
     def __init__(self, description, length: int, width: int, height: int):
+        '''The box is three-dimensional and has three values to consider when calculation surface area and volume'''
         self.length = length
         self.width = width
         self.heigth = height
@@ -101,17 +110,22 @@ class Box(ThreeDimensionalShape):
         return (self.length * self.width) * self.heigth
 
 
-
+# Creating shape objects:
 square1 = Square('Squares have all equal sides', 7)
 rectangle1 = Rectangle('A rectangle has unequal side lenghts', 12, 6)
 cube1 = Cube('this object is three-dimensional', 13)
 box1 = Box('three-dimensional, but with 3 different sides', 12,9,10)
 
+# Adding objects to a list
 obj_list = [square1, rectangle1, cube1, box1]
 
 def main():
+    '''main function, contains all function calls and statements of the program'''
     for obj in obj_list:
         print(obj)
     print(Shape._counter)
 
+
+# A statement that makes the processor load all lines of code into memory before calling main() function
 if __name__ == '__main__': main()
+
