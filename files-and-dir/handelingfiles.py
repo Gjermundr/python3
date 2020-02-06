709cc0cc8f19b78d0469175ae5fe729de9cd514e
gjermund.haugen <gjermund.f.haugen@gmail.com>
2020-02-06 11:05:57 +0100

    new files

---
+++ files-and-dir/handelingfiles.py
@@ -0,0 +1,39 @@
+#!#/usr/bin/env python3
+import argparse
+
+parser = argparse.ArgumentParser(description='Lots of dumb stuff')
+parser.add_argument('-f', '--file', type=str, help='Cat a file to output')
+parser.add_argument('-p', '--pikachu', action='store_true', help='Do you want a Pikachu?')
+args = parser.parse_args()
+
+
+def d_pikachu():
+    print('''
+█▀▀▄░░░░░░░░░░░▄▀▀█
+░█░░░▀▄░▄▄▄▄▄░▄▀░░░█
+░░▀▄░░░▀░░░░░▀░░░▄▀
+░░░░▌░▄▄░░░▄▄░▐▀▀
+░░░▐░░█▄░░░▄█░░▌▄▄▀▀▀▀█
+░░░▌▄▄▀▀░▄░▀▀▄▄▐░░░░░░█
+▄▀▀▐▀▀░▄▄▄▄▄░▀▀▌▄▄▄░░░█
+█░░░▀▄░█░░░█░▄▀░░░░█▀▀▀
+░▀▄░░▀░░▀▀▀░░▀░░░▄█▀
+░░░█░░░░░░░░░░░▄▀▄░▀▄
+░░░█░░░░░░░░░▄▀█░░█░░█
+░░░█░░░░░░░░░░░█▄█░░▄▀
+░░░█░░░░░░░░░░░████▀
+░░░▀▄▄▀▀▄▄▀▀▄▄▄█▀
+    ''')
+
+
+
+def main():
+    if args.file:
+        file = open(args.file)
+        print(file.read())
+
+    if args.pikachu:
+        d_pikachu()
+
+
+if __name__ == '__main__': main()
